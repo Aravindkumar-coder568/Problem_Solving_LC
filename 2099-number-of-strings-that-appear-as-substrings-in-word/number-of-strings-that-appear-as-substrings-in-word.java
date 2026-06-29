ef1df47c6c1ed8b9f36fa582185c0334fae607cd
Aravindkumar-coder568 <aravindmariappan4@gmail.com>
2026-06-29 16:19:46 +0530

    Time: 1 ms (75.75%) | Memory: 43.4 MB (56.03%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,25 +1,25 @@
 class Solution {
-    // public static int isContains(,String sub){
-    //     int n= arr.length;
-    //     for(int i=0;i<n;i++){
-
-    //     }
-    // }
     public int numOfStrings(String[] patterns, String word) {
         int n=patterns.length;
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String s : patterns) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
+        // HashMap<String, Integer> map = new HashMap<>(); -
+        // for (String s : patterns) {                        
+        //     map.put(s, map.getOrDefault(s, 0) + 1);
+        // }
+        // int count=0;
+        // int index=0;                                       => It's also correct 
+        // for(int i=0;i<word.length();i++){                   
+        //     for(int j=i+1;j<=word.length();j++){              
+        //         String sub=word.substring(i,j);
+        //         if(map.containsKey(sub)){
+        //             count+=map.get(sub);
+        //             map.remove(sub);
+        //         }
+        //     }
+        // }
         int count=0;
-        int index=0;
-        for(int i=0;i<word.length();i++){
-            for(int j=i+1;j<=word.length();j++){
-                String sub=word.substring(i,j);
-                if(map.containsKey(sub)){
-                    count+=map.get(sub);
-                    map.remove(sub);
-                }
+        for(int i=0;i<patterns.length;i++){
+            if(word.contains(patterns[i])){
+                count++;
             }
         }
         
